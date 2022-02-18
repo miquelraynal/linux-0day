cd8cfe6457021139da0166cd05a488b5b7b4bd00
Phil Edworthy <phil.edworthy@renesas.com>
2022-02-18 12:07:27 +0100

    serial: 8250_dw: Move the per-device structure
    This structure needs to be reused from dwlib, so let's move it into a
    shared header. There is no functional change.
    
    Signed-off-by: Phil Edworthy <phil.edworthy@renesas.com>
    [miquel.raynal@bootlin.com: Extracted from a bigger change]
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- drivers/tty/serial/8250/8250_dwlib.h
+++ drivers/tty/serial/8250/8250_dwlib.h
@@ -16,5 +16,21 @@ struct dw8250_port_data {
 	u8			dlf_size;
 };
 
+struct dw8250_data {
+	struct dw8250_port_data	data;
+
+	u8			usr_reg;
+	int			msr_mask_on;
+	int			msr_mask_off;
+	struct clk		*clk;
+	struct clk		*pclk;
+	struct notifier_block	clk_notifier;
+	struct work_struct	clk_work;
+	struct reset_control	*rst;
+
+	unsigned int		skip_autocfg:1;
+	unsigned int		uart_16550_compatible:1;
+};
+
 void dw8250_do_set_termios(struct uart_port *p, struct ktermios *termios, struct ktermios *old);
 void dw8250_setup_port(struct uart_port *p);
